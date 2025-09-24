ed695536f188dba0cb6162ea55c0b1614887abfd
toki-ai <lucngan2004@gmail.com>
2025-09-24 08:37:58 +0700

    update bucket

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyDv7goA31q-pu1VJssu_V8-2HBGSArMGKk',
   authDomain: 'phiso-quizzi.firebaseapp.com',
   projectId: 'phiso-quizzi',
-  storageBucket: 'phiso-quizzi.firebasestorage.app',
+  storageBucket: 'phiso-quizzi.appspot.com',
   messagingSenderId: '690755271088',
   appId: '1:690755271088:web:671f8b2b55c27125c4db02',
 }
